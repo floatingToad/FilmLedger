eeb68c03f07e80c696a7b592b1eab603465bc139
derri4l <dsimons0001@gmail.com>
2026-02-23 21:45:17 -0600

    did test insert

--- testimport.py
+++ testimport.py
@@ -32,9 +32,31 @@ except Exception as e:
 url = f"https://api.themoviedb.org/3/movie/popular?api_key={TMDB_API_KEY}"
 response = requests.get(url)
 
-print("Status code:", response.status_code)
+if response.status_code != 200:
+    print("TMDB API error:", response.text)
+    exit(1)
 
 data = response.json()
+movie = data["results"][0] 
 
-print("Keys:", data.keys())
-print("First movie:", data["results"][0])          
+tmdb_id = movie["id"]
+title = movie["title"]
+release_date = movie.get("release_date")
+description = movie.get("overview")
+
+#insert into movies table, if id exist update the record
+cur.execute("""
+    INSERT INTO movies (tmdb_id, title, release_date, description)
+    VALUES (%s, %s, %s, %s)
+    ON CONFLICT (tmdb_id)            
+    DO UPDATE SET     
+        title = EXCLUDED.title,
+        release_date = EXCLUDED.release_date,
+        description = EXCLUDED.description;
+""", (tmdb_id, title, release_date, description))
+
+conn.commit()
+print(f"Done!")
+
+cur.close()
+conn.close()
