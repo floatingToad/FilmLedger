91956e3e10ed722a0a328d2ed4183fa9a592298f
derri4l <dsimons0001@gmail.com>
2026-02-26 02:02:05 -0600

    insert all tmdb genres

--- import_genre.py
+++ import_genre.py
@@ -13,3 +13,26 @@ params = {
     "api_key": TMBD_API_KEY,
 }
 response = requests.get(url, params=params)
+if response.status_code != 200:
+    print("failed.", response.text)
+    exit(1)
+
+# parse response
+data = response.json()
+genres = data["genres"]
+
+#insert genres, if ID exist update the record
+with get_connection() as conn:
+    with conn.cursor() as cur:
+        for genre in genres:
+            cur.execute("""
+            INSERT INTO genres (tmbd_id, genre)
+            VALUES (%s, %s)
+            ON CONFLICT (tmbd_id)
+            DO UPDATE SET name = EXCLUDED.name;  
+            """, (genre["id"], genre["name"]))
+    conn.commit()
+print("Done!")
+
+
+        
